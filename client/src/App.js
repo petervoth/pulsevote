98b37274f0aa9ecb81e324ce957314ba991505bf
Peter Voth <petervoth@gmail.com>
2025-10-23 20:42:39 -0400

    Another bounds adjustment: 12

--- client/src/App.js
+++ client/src/App.js
@@ -354,7 +354,7 @@ function ChoroplethLayer({ points }) {
 
         const updateGrid = () => {
             const bounds = map.getBounds();
-            const geoJSON = generateGridGeoJSON(bounds, 8); // Adjust grid size here (default: 6)
+            const geoJSON = generateGridGeoJSON(bounds, 12); // Adjust grid size here (default: 12)
             setGridGeoJSON(geoJSON);
             setMapKey(prev => prev + 1);
         };
